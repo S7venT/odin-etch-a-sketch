983f25e2ed445da66d2fd57a179971166796ce3d
S7venT <s7ventr@gmail.com>
2022-11-24 17:15:58 -0500

    add js file

---
+++ main.js
@@ -0,0 +1,14 @@
+function createBox(size) {
+    let board = document.querySelector(".box");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
